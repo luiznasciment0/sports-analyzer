1a47e0f6b2ff1d86507bcbe61d72360a961c3d8d
Luiz Nascimento <luizp.nascimento@outlook.com>
2021-04-03 16:07:58 -0300

    rename things and improve code

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,10 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 import puppeteer from 'puppeteer'
 
-// const getfirstHalfCards = async () => {
-//   const browser = await puppeteer.launch()
-//   const page = await browser.newPage()
-//   await page.goto('https://www.scorebing.com/league/35')
-
-//   const firstHalfCards = await page.evaluate(() => {
-//     const cards = []
-//     document.querySelectorAll('ul#race_events.pricing-table > li').forEach((card) => cards.push(card.textContent.replace(/[\n\r]+|[\s]{2,}/g, ' ').trim()))
-//     return cards
-//   })
-
-//   browser.close()
-//   return firstHalfCards
-// }
-
-// getfirstHalfCards().then((value) => {
-//   console.log(value)
-// })
-
-const getAllMatches = async () => {
+const getLastRoundMatches = async (leagueUrlAddress = 'https://www.scorebing.com/league/35') => {
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
-  await page.goto('https://www.scorebing.com/league/35')
-
+  await page.goto(leagueUrlAddress)
 
   const matchesURLAddress = await page.evaluate(() => {
     const links = []
@@ -33,14 +13,34 @@ const getAllMatches = async () => {
     matchesLinks.forEach((item) => links.push(item.href))
     return links
   })
-
   browser.close()
   return matchesURLAddress
 }
 
-getAllMatches().then((value) => {
-  console.log(value)
-})
+const getMatchEvents = async (matchUrlAddress = 'https://www.scorebing.com/match_live/827460') => {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  await page.goto(matchUrlAddress)
 
+  const events = await page.evaluate(() => {
+    const cards = []
+    document.querySelectorAll('ul#race_events.pricing-table > li').forEach((card) => cards.push(card.textContent.replace(/[\n\r]+|[\s]{2,}/g, ' ').trim()))
+    return cards
+  })
+
+  browser.close()
+  return events
+}
+
+const lastRoundEvents = async () => {
+  const last10Matches = await getLastRoundMatches('https://www.scorebing.com/league/35')
+  last10Matches.forEach(async (match) => {
+    await getMatchEvents(match)
+  })
+  console.log(last10Matches)
+  return
+}
+
+console.log(lastRoundEvents())
 // pegar urls de todas as partidas das ultimas 10 rodadas e guardar
 // acessar uma por uma e pegar a tabela de eventos
